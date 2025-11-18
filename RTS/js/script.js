32ab18d723526b7003c206e8bf6a5e3e0d9adb4e
EhabSSarrawi <ehab-200-0@hotmail.com>
2025-11-18 20:58:14 +0200

    Second

--- RTS/js/script.js
+++ RTS/js/script.js
@@ -11,23 +11,15 @@ const navbar = document.querySelector("[data-navbar]");
 const navCloseBtn = document.querySelector("[data-nav-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
 
-const elemArr = [navCloseBtn, overlay, navOpenBtn];
-
-for (let i = 0; i < elemArr.length; i++) {
-  elemArr[i].addEventListener("click", function () {
-    navbar.classList.toggle("active");
-    overlay.classList.toggle("active");
-  });
+function toggleMenu() {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-open");
 }
 
-const navbarLinks = document.querySelectorAll("[data-navbar-link]");
-
-for (let i = 0; i < navbarLinks.length; i++) {
-  navbarLinks[i].addEventListener("click", function () {
-    navbar.classList.toggle("active");
-    overlay.classList.toggle("active");
-  });
-}
+navOpenBtn.addEventListener("click", toggleMenu);
+navCloseBtn.addEventListener("click", toggleMenu);
+overlay.addEventListener("click", toggleMenu);
 
 
 
